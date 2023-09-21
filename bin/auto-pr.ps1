ff17695577700074f7824c970318373bf84caf34
SirRobbe <103939713+SirRobbe@users.noreply.github.com>
2023-09-21 23:25:48 +0200

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "SirRobbe/scoop-sirrobbe:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
